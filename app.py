bece4a2f1204b5347f1351fca07b02cb7cca6a24
Souza <augustosouza@users.noreply.huggingface.co>
2025-02-21 16:33:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -46,12 +46,13 @@ def calculator(a: int, b: int) -> int:
 final_answer = FinalAnswerTool()
 
 # If the agent does not answer, the model is overloaded, please use another model or the following Hugging Face Endpoint that also contains qwen2.5 coder:
-# model_id='https://pflgm2locj2t89co.us-east-1.aws.endpoints.huggingface.cloud' 
+model_id='https://pflgm2locj2t89co.us-east-1.aws.endpoints.huggingface.cloud' 
 
 model = HfApiModel(
 max_tokens=2096,
 temperature=0.5,
-model_id='Qwen/Qwen2.5-Coder-32B-Instruct',# it is possible that this model may be overloaded
+model_id='https://pflgm2locj2t89co.us-east-1.aws.endpoints.huggingface.cloud',
+# model_id='Qwen/Qwen2.5-Coder-32B-Instruct',# it is possible that this model may be overloaded
 custom_role_conversions=None,
 )
 
